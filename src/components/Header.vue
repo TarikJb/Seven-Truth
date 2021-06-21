7700727a88d7f319bc2c55e6d220b8c075a25a4b
Tarik Jebrouni <Tarik.hani74@gmail.com>
2021-06-21 21:18:50 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- Header -->
+  <div class="container-fluid">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/icon.png" width="30" height="30" />
+        7 Truth about you</a
+      >
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+.navbar-light .navbar-brand {
+  color: white;
+  font-size: 18px;
+  font-weight: 800 !important;
+  line-height: 10px;
+  word-spacing: 0px;
+}
+.bg-light {
+  background-color: #c313b6 !important;
+  height: 40px;
+  padding: 0px;
+}
+.navbar-light .navbar-brand:hover {
+  color: white;
+}
+.navbar-light .navbar-brand img {
+  margin-left: 10px;
+}
+
+.container-fluid {
+  padding: 0px;
+  margin: 0px;
+  max-width: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container text-center">
+    <div class="body">
+      <h1 class="h1">Dare For your Friends</h1>
+      <h3 class="h3">7 Truths about you</h3>
+    </div>
+    <div class="lang">
+      <label>Select Language :</label><br />
+      <select class="sl browser-default custom-select">
+        <option value="en">English</option>
+        <option value="ar">عربي</option>
+      </select>
+    </div>
+    <label>Your Name :</label><br />
+    <div class="name">
+      <input type="text" class="form-control" placeholder="Full Name" />
+    </div>
+    <div class="btn bn-block">
+      <button class="btn btn-default">👉&nbsp;Register&nbsp;👈</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'Home',
+  components: {},
+}
 </script>
+<style scoped>
+.sl {
+  max-width: 60%;
+}
+.lang {
+  margin-top: 50px;
+}
+.form-control {
+  width: 75%;
+}
+.name {
+  margin-left: 20%;
+}
+.btn {
+  margin: 0px auto;
+  margin-top: 2px;
+  width: 79%;
+}
+.btn-default {
+  color: white;
+  background-color: #d00a6f;
+}
+
+.btn:hover,
+.btn-default :hover {
+  color: white !important;
+}
+</style>
